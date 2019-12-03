73efe387e64b669d4ccd52832e4eacf195527e90
kima111 <kima111@gmail.com>
2019-12-02 19:35:31 -0800

    update

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Pineapple');
+INSERT INTO burgers (burger_name) VALUES ('Double Stack');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chesseburger', true);
